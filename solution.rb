0ef91519c59ec0357c6e5a8e9f9cc170b4f41597
Diego Ospina <diegospinar@gmail.com>
2019-06-15 17:59:37 -0500

    Ticket 2 Change

--- solution.rb
+++ solution.rb
@@ -3,10 +3,13 @@ require "sinatra"
 get '/' do
     unless params[:nombre]
         "<h1>Hola desconocido!<h1>"  
-    
     else
-        "<h1> Hola #{params[:nombre].capitalize}!</h1>"   
-    end    
+        if params[:nombre].empty?
+        "<h1>Hola desconocido!<h1>" 
+        else
+        "<h1> Hola #{params[:nombre].capitalize}!</h1>"     
+        end
+    end
 end
    
 
